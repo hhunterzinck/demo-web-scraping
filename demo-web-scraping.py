aa0f0b7f7d7750a28be2b164126fbe5ed6fbde11
hhunterzinck <haleyhunterzinck@gmail.com>
2022-06-01 12:32:56 -0700

    regex

--- demo-web-scraping.py
+++ demo-web-scraping.py
@@ -1,9 +1,11 @@
 # Description: 
 # Author: Haley Hunter-Zinck
-# Date: 2022-
+# Date: 2022-06-01
 # tutorial: https://realpython.com/python-web-scraping-practical-introduction/
 
 from urllib.request import urlopen
+
+import re
 
 # paramters
 url_aphrodite = "http://olympus.realpython.org/profiles/aphrodite"
@@ -49,3 +51,32 @@ start_index = title_index + len("<title >")
 end_index = html.find("</title>")
 title = html[start_index:end_index]
 print(f"title take 2: '{title}'")
+
+######################
+#       regex        #
+######################
+
+# asterisk (*) behavior: zero or more of previous character
+re.findall("ab*c", "ac")
+re.findall("ab*c", "abcd")
+re.findall("ab*c", "acc")
+re.findall("ab*c", "abcac")
+re.findall("ab*c", "abdc")
+re.findall("ab*c", "ABC")
+re.findall("ab*c", "ABC", re.IGNORECASE)
+
+# period (.) behavior: any single character
+re.findall("a.c", "abc")
+re.findall("a.c", "abbc")
+re.findall("a.c", "ac")
+re.findall("a.c", "acc")
+
+# combos of period and asterisk
+re.findall("a.*c", "abc")
+re.findall("a.*c", "abbc")
+re.findall("a.*c", "ac")
+re.findall("a.*c", "acc")
+
+# search function
+match_results = re.search("ab*c", "ABC", re.IGNORECASE)
+match_results.group()
